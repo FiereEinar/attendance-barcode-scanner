4176275a617788009298c90a1458b597a27e4470
Nick Mours <nickxylanmelloria@gmail.com>
2025-02-03 13:21:58 +0800

    added results in the page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,33 @@
 import { useState } from 'react';
 import Html5QrcodePlugin from './components/Html5QrcodePlugin';
-import { Button } from './components/ui/button';
 
 function App() {
-	const [show, setShow] = useState(false);
+	const [results, setResults] = useState<{ [key: string]: string }>({});
 	const onNewScanResult = (decodedText: string) => {
 		console.log(`Scan decoded text: ${decodedText}`);
+		if (!results[decodedText]) {
+			setResults({ ...results, [decodedText]: decodedText });
+		}
 	};
 
 	return (
-		<main className='p-5'>
-			<Button onClick={() => setShow(true)}>Show</Button>
-			<div className='size-[1000px]'>
-				{show && (
-					<Html5QrcodePlugin
-						fps={10}
-						qrbox={{ width: 1000, height: 1000 }}
-						disableFlip={false}
-						qrCodeSuccessCallback={onNewScanResult}
-					/>
+		<main className='p-5 flex flex-col items-center gap-12'>
+			<div className='size-[500px]'>
+				<Html5QrcodePlugin
+					fps={10}
+					qrbox={{ width: 500, height: 500 }}
+					disableFlip={false}
+					qrCodeSuccessCallback={onNewScanResult}
+				/>
+			</div>
+			<div>
+				{results && (
+					<ul>
+						<p className='text-center'>results</p>
+						{Object.keys(results).map((key) => (
+							<li key={key}>{results[key]}</li>
+						))}
+					</ul>
 				)}
 			</div>
 		</main>
